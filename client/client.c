29270325d7eb776ed8f9854d39b0b9384b037f1d
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2020-10-20 19:47:31 -0500

    Client program

--- client/client.c
+++ client/client.c
@@ -8,32 +8,51 @@
 #include <strings.h>
 #include <sys/socket.h>
 
-typedef struct td {
-	int hid;
-	char *msg;
-} ThreadData;
-
 // Compile and link with ptread
-void *hello(void *data) {
-  ThreadData *td = (ThreadData *)data;
-  printf("%s %d\n", td->msg, td->hid);
-  free(data);
+void *listener(void *socket) {
+
+	int socketfd = *(int*)socket;
+
+	char message[255];
+	while (read(socketfd, &message, sizeof(message))) {
+		/* write(fileno(stdout), &message, sizeof(message)); */
+		printf("%s", message);
+	}
   pthread_exit(NULL);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc < 4) {
+		printf("Invalid number of argumet");
+		return 1;
+	}
+
 	struct hostent *serverinfo = gethostbyname(argv[1]);
 	struct sockaddr_in serverAddress;
 
 	bzero(&serverAddress, sizeof(serverAddress));
 	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(2727);
-	pthread_t newThread;
-	for (int i = 0; i < 10; i++) {
-		ThreadData *td = (ThreadData *)malloc(sizeof(ThreadData));
-		td->msg = "Hello Thread";
-		td->hid = i;
-		pthread_create(&newThread, NULL, hello, (void *)td);
+	serverAddress.sin_port = htons(atoi(argv[2]));
+
+	bcopy((char *)serverinfo->h_addr, (char *)&serverAddress.sin_addr, serverinfo->h_length);
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	connect(sockfd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+	char *username = argv[3];
+	write(sockfd, &username, sizeof(username));
+
+	pthread_t lis;
+	pthread_create(&lis, NULL, listener, (void*)&sockfd);
+
+	char message[255];
+
+	while (read(fileno(stdin), &message, sizeof(message))) {
+		write(sockfd, &message, sizeof(message));
+		if (strcmp(message, "bye")) {
+			close(sockfd);
+			return 0;
+		}
 	}
+
 	pthread_exit(NULL);
 }
